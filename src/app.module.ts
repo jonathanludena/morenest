928288b6d10844541df51be824ddb550457c53f7
jonathanludena <jonathanludenai@gmail.com>
2022-09-10 22:34:09 -0500

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getEnvPath } from './common/helper/env.helper';
+import { ItemsModule } from './items/items.module';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [],
+  imports: [
+    ItemsModule,
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      authMechanism: 'DEFAULT',
+      dbName: process.env.DB_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Items test API')
+    .setDescription('Description API')
+    .setVersion('1.0')
+    .addTag('items')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
+  console.log('object', process.env.URI_DB);
+
   await app.listen(3000);
 }
 bootstrap();
